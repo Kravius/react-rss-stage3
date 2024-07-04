aa0a72968b020d987aff9becf14a9f2c1b19b5b1
Glib Shemenkov <shemenkovglib@gmail.com>
2024-07-04 18:44:45 +0200

    chore:basic configurations

---
+++ vite-project/.prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  printWidth: 120,
+  arrowParens: "always",
+  endOfLine: "auto",
+};
